f1da3342815213eb8a6333da42132e6afe55bcf9
DaggerHashimmoto <DaggerHashimmoto@gmail.com>
2022-08-09 16:46:39 +0500

    Ready?

---
+++ js/script.js
@@ -0,0 +1,67 @@
+$(window).on('load', function (){
+    $('#header').vide('./video/cover', {
+        bgColor: '#1d1d1a'
+    })
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function sum (a, b) {
+//     return a + b
+// }
+//
+// function dif (a, b) {
+//     return a - b
+// }
+//
+//  function dosome(func) {
+//     let x, y;
+//         x = 348;
+//         y = 476;
+//    return func(x, y)
+//  }
+//
+// console.log(dosome(sum))
+// console.log(dosome(dif))
+//
+// let res = (function (a, b) {
+//     return a * b
+// })
+//
+// (25, 75)
+//
+// console.log(res)
+//
+// // function sayHi(name) {
+// //     console.log(`Hello ${name}!`)
+// // }
+// //
+// // sayHi('Motto')
+//
+// let arrSayHi = (name) => {
+//     console.log(`Hello ${name}!`)
+// }
+//
+// arrSayHi(`Motto`)
